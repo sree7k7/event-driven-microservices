624f91c9732afa534102d4762e61063040081243
Srikanth <sree7k7@gmail.com>
2026-04-05 16:17:29 +0200

    Added: Unit test fix

--- tests/unit/test_event_driven_microservices_stack.py
+++ tests/unit/test_event_driven_microservices_stack.py
@@ -1,15 +1,15 @@
-# import aws_cdk as core
-# import aws_cdk.assertions as assertions
+import aws_cdk as core
+import aws_cdk.assertions as assertions
 
-# from event_driven_microservices.event_driven_microservices_stack import EventDrivenMicroservicesStack
+from event_driven_microservices.event_driven_microservices_stack import EventDrivenMicroservicesStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in event_driven_microservices/event_driven_microservices_stack.py
-# def test_sqs_queue_created():
-#     app = core.App()
-#     stack = EventDrivenMicroservicesStack(app, "event-driven-microservices")
-#     template = assertions.Template.from_stack(stack)
+def test_sqs_queue_created():
+    app = core.App()
+    stack = EventDrivenMicroservicesStack(app, "event-driven-microservices")
+    template = assertions.Template.from_stack(stack)
 
-# #     template.has_resource_properties("AWS::SQS::Queue", {
-# #         "VisibilityTimeout": 300
-# #     })
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
